163a6af782b8dd3651c2e5ace530d94f922c4811
Kevin Pan <kevinrpan@gmail.com>
2022-12-30 11:08:52 -0600

    send in everything

--- pages/index.js
+++ pages/index.js
@@ -23,8 +23,8 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
+// const analytics = getAnalytics(app);
+const fs = require('fs');
 
 const Home = () => {
   const [userInput, setUserInput] = useState('');
